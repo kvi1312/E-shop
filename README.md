132d20807575669873acf2f6dd36843619a11532
khai.le <khai.le@ptnglobalcorp.com>
2025-03-05 16:54:39 +0700

    Handle product and category module

--- README.md
+++ README.md
@@ -23,4 +23,4 @@ Things you may want to cover:
 
 - ...
 
-admin account : lenguyenkhai2611@gmail.com - a2601197
+admin account : lenguyenkhai2611@gmail.com - 123456
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Admin.find_or_create_by!(email: "lenguyenkhai2611@gmail.com") do |admin|
+  admin.password = "123456"
+  admin.password_confirmation = "123456"
+end
