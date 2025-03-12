3bb9aa130f16385baa1a1bb268e0cb5c96f89db3
khai.le <khai.le@ptnglobalcorp.com>
2025-03-12 15:08:42 +0700

    set up deploy render

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -7,40 +7,46 @@ class WebhooksController < ApplicationController
     payload = request.body.read
     sig_header = request.env["HTTP_STRIPE_SIGNATURE"]
     endpoint_secret = Rails.application.credentials.dig(:stripe, :webhook_secret_key)
+    puts "Key: #{endpoint_secret}"
     event = nil
 
     begin
       event = Stripe::Webhook.construct_event(payload, sig_header, endpoint_secret)
-    rescue JSON::ParserError => error
+    rescue JSON::ParserError => e
       status 400
       return
-    rescue Stripe::SignatureVerificationError => error
-      puts "Webhook signature verification FAILED: #{error.message}"
+    rescue Stripe::SignatureVerificationError => e
+      puts "Webhook signature verification failed."
       status 400
       return
     end
 
     case event.type
-    when "checkout.session.completed"
+    when 'checkout.session.completed'
       session = event.data.object
       shipping_details = session["shipping_details"]
-      address = "#{shipping_details["address"]["line1"]} #{shipping_details["address2"]["city"]}, #{shipping_details["address"]["state"]} #{shipping_details["address"]["postal_code"]}"
+      puts "Session: #{session}"
+      if shipping_details
+        address = "#{shipping_details['address']['line1']} #{shipping_details['address']['city']}, #{shipping_details['address']['state']} #{shipping_details["address"]["postal_code"]}"
+      else
+        address = ""
+      end
       order = Order.create!(customer_email: session["customer_details"]["email"], total: session["amount_total"], address: address, fulfilled: false)
       full_session = Stripe::Checkout::Session.retrieve({
                                                           id: session.id,
-                                                          expand: [ "line_items" ]
+                                                          expand: ['line_items']
                                                         })
       line_items = full_session.line_items
       line_items["data"].each do |item|
         product = Stripe::Product.retrieve(item["price"]["product"])
         product_id = product["metadata"]["product_id"].to_i
         OrderProduct.create!(order: order, product_id: product_id, quantity: item["quantity"], size: product["metadata"]["size"])
-        Stock.find(product["metadata"]["stock_id"]).decrement!(:amount, item["quantity"])
+        Stock.find(product["metadata"]["product_stock_id"]).decrement!(:amount, item["quantity"])
       end
     else
       puts "Unhandled event type: #{event.type}"
     end
 
-    render json: { message: "success" }
+    render json: { message: 'success' }
   end
 end
--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
             div.innerText = `Item: ${item.name} - $${item.price / 100.0} - Size: ${item.size} - Quantity: ${item.quantity}`
             const deleteButton = document.createElement("button")
             deleteButton.innerText = "Remove";
-            deleteButton.value = item.id;
+            deleteButton.value = JSON.stringify( {id: item.id, size: item.size})
             deleteButton.classList.add("bg-gray-500", "rounded", "text-white", "px-2", "py-1", "ml-2")
             deleteButton.addEventListener("click", this.removeFromCart)
             div.appendChild(deleteButton)
@@ -35,8 +35,9 @@ export default class extends Controller {
 
     removeFromCart(e) {
         const cart = JSON.parse(localStorage.getItem("cart"))
-        const id = e.target.value
-        const index = cart.findIndex(item => item.id === id)
+        const values = JSON.parse( e.target.value)
+        const {id, size} = values
+        const index = cart.findIndex(item => item.id === id && item.size === size)
         cart.splice(index, 1)
         localStorage.setItem("cart", JSON.stringify(cart))
         window.location.reload()
